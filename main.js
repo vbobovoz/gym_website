84d5d4c4ad3850f45edd2a124f1cd070db826352
Vladislav Bobovoz <vladislav.bobovoz@outlook.com>
2023-05-15 09:34:19 +0300

    [add]: sign in, sign up, directions

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    PopUpHide1();
+});
+function PopUpShow1(){
+    $("#popup1").show();
+}
+function PopUpHide1(){
+    $("#popup1").hide();
+}
+
+function PopUpShow2(){
+    $("#popup2").show();
+}
+function PopUpHide2(){
+    $("#popup2").hide();
+}
